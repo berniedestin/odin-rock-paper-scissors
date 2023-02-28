d8c1fd2cb819c69620b51a3c3d9282f0aa0edd35
Destin <berniedestin7@gmail.com>
2023-02-28 10:07:43 -0500

    Create makeLowecase function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getComputerChoice(){
+    // This function returns a randomly selected
+    // rock, paper, or scissors
     let numChoice = Math.floor((Math.random()*3) +1)
     if ( numChoice == 1 ) {
         return "rock"
@@ -7,6 +9,14 @@ function getComputerChoice(){
     } else {
         return "scissors"
     }
+}
+
+function playRound(userInput) {
 
 }
 
+function makeLowercase(string) {
+    return (typeof string === "string") ? 
+    string.toLowerCase() : 
+    "Please enter a stirng!"
+}
